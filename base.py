61749e67da8fba50658fa2f6422972b820ef142b
qualv13 <71326761+qualv13@users.noreply.github.com>
2025-04-18 18:59:21 +0200

    last clean-up

--- base.py
+++ base.py
@@ -3,19 +3,18 @@ import pytz
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
 LOCAL_TIMEZONE = pytz.timezone('Europe/Warsaw')
 
 
-class Asset(db.Model): # Majątek
-  __tablename__ = 'assets'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50), nullable=False)
-  value = db.Column(db.Integer, nullable=False)
-  currency = db.Column(db.String(3), nullable=False)
-  is_pln = db.Column(db.Boolean, default=False)
-  time_created = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(LOCAL_TIMEZONE))
-  time_updated = db.Column(db.DateTime(timezone=True), onupdate=lambda: datetime.now(LOCAL_TIMEZONE))
+class Asset(db.Model):  # Majątek
+    __tablename__ = 'assets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    currency = db.Column(db.String(3), nullable=False)
+    is_pln = db.Column(db.Boolean, default=False)
+    time_created = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(LOCAL_TIMEZONE))
+    time_updated = db.Column(db.DateTime(timezone=True), onupdate=lambda: datetime.now(LOCAL_TIMEZONE))
 
 
 class Rate(db.Model):
